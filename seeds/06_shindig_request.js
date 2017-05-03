3e83635a1f95958f888d7433d9f8f204a63c7933
Jeff Miguel Hernandez <jeffhernandez89@gmail.com>
2017-05-03 15:40:08 -0600

    request route added

--- seeds/06_shindig_request.js
+++ seeds/06_shindig_request.js
@@ -6,12 +6,16 @@ exports.seed = function(knex, Promise) {
       // Inserts seed entries
       return knex('shindig_request').insert([
         { shindig_id: 1, user_id: 1 },
-        { shindig_id: 2, user_id: 2 },
-        { shindig_id: 3, user_id: 3 },
+        { shindig_id: 4, user_id: 2 },
+        { shindig_id: 7, user_id: 3 },
+        { shindig_id: 5, user_id: 4 },
+        { shindig_id: 5, user_id: 2 },
+        { shindig_id: 5, user_id: 6 },
+        { shindig_id: 1, user_id: 3 },
         { shindig_id: 4, user_id: 4 },
         { shindig_id: 5, user_id: 2 },
         { shindig_id: 6, user_id: 6 },
-        { shindig_id: 7, user_id: 3 },
+        { shindig_id: 4, user_id: 3 },
 
       ]);
     });
