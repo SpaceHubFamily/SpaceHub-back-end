7dda999bf09e639dc565fea80722e26b61c8d4dd
Jeff Miguel Hernandez <jeffhernandez89@gmail.com>
2017-05-04 13:58:16 -0600

    shindig id added to shindig_request/use/#

--- routes/shindig_request.js
+++ routes/shindig_request.js
@@ -2,12 +2,65 @@ const router = require('express').Router();
 const knex = require('../db/knex');
 
 router.get('/', function(req, res) {
-  knex('shindig_request')
-  .select()
-  .then(function (result) {
+  if (req.session.userId) {
+  return knex('shindig_request')
+  .join('shindig', 'shindig_request.shindig_id', '=', 'shindig.id')
+  .join('users', 'users.id', '=', 'shindig_request.user_id')
+  .join('room', 'room.id', '=', 'shindig.room_id')
+  .join('venue', 'venue.id', '=', 'room.venue_id')
+  .select('shindig.date as requested_date', 'shindig.start_time',
+    'shindig.end_time', 'shindig.available', 'shindig.user_id as owner_id',
+    'users.first_name as requester_first_name',
+    'users.last_name as requester_last_name',
+    'users.img_url as requester_img_url',
+    'users.phone_number as requester_phone_number',
+    'users.company_name as requester_company_name',
+    'users.email as requester_email',
+    'users.description as requester_description',
+    'room.name as room_name', 'room.capacity', 'room.hour_rate',
+    'room.day_rate', 'room.img_url as room_img_url',
+    'venue.address as venue_address', 'venue.address as venue_address',
+    'venue.city', 'venue.state', 'venue.zip_code', 'venue.name as venue_name',
+    'venue.email as venue_email', 'venue.phone as venue_phone',
+    'venue.description as venue_description', 'venue.img_url as venue_img_url')
+  .where('shindig.user_id', req.session.userId)
+  .then(function(result) {
     res.json(result);
   })
-});
+}
+return res.status(401).send('not logged in')
+})
+
+// router.get('/', function(req, res) {
+//   if(req.session.userId){
+//     return knex('shindig_request')
+//     .join('shindig', 'shindig_request.shindig_id', '=', 'shindig.id')
+//     .join('users', 'users.id', '=', 'shindig_request.user_id')
+//     .join('room', 'room.id', '=', 'shindig.room_id')
+//     .join('venue', 'venue.id', '=', 'room.venue_id')
+//     .select('shindig.date as requested_date', 'shindig.start_time',
+//       'shindig.end_time', 'shindig.available', 'shindig.user_id as owner_id',
+//       'users.first_name as requester_first_name',
+//       'users.last_name as requester_last_name',
+//       'users.img_url as requester_img_url',
+//       'users.phone_number as requester_phone_number',
+//       'users.company_name as requester_company_name',
+//       'users.email as requester_email',
+//       'users.description as requester_description',
+//       'room.name as room_name', 'room.capacity', 'room.hour_rate',
+//       'room.day_rate', 'room.img_url as room_img_url',
+//       'venue.address as venue_address', 'venue.address as venue_address',
+//       'venue.city', 'venue.state', 'venue.zip_code', 'venue.name as venue_name',
+//       'venue.email as venue_email', 'venue.phone as venue_phone',
+//       'venue.description as venue_description', 'venue.img_url as venue_img_url')
+//     .where('shindig_request.user_id', req.params.userId)
+//     .then(function (result) {
+//       res.json(result);
+//     })
+//   }
+//   return res.status(401).send('not logged in')
+// });
+
 let myObject;
 router.get('/:id', function(req, res) {
     knex('shindig_request')
@@ -30,14 +83,14 @@ router.get('/:id', function(req, res) {
       'venue.city', 'venue.state', 'venue.zip_code', 'venue.name as venue_name',
       'venue.email as venue_email', 'venue.phone as venue_phone',
       'venue.description as venue_description', 'venue.img_url as venue_img_url')
-    .where('shindig_request.shindig_id', req.params.id)
+    .where('shindig_request.user_id', req.params.userId)
     .then(function (result) {
       res.json(result);
     })
 })
 
 router.get('/user/:id', function(req, res) {
-    knex('shindig_request')
+    return knex('shindig_request')
     .join('shindig', 'shindig_request.shindig_id', '=', 'shindig.id')
     .join('users', 'users.id', '=', 'shindig_request.user_id')
     .join('room', 'room.id', '=', 'shindig.room_id')
@@ -56,7 +109,8 @@ router.get('/user/:id', function(req, res) {
       'venue.address as venue_address', 'venue.address as venue_address',
       'venue.city', 'venue.state', 'venue.zip_code', 'venue.name as venue_name',
       'venue.email as venue_email', 'venue.phone as venue_phone',
-      'venue.description as venue_description', 'venue.img_url as venue_img_url')
+      'venue.description as venue_description', 'venue.img_url as venue_img_url',
+      'shindig.id as shindig_id')
     .where('shindig_request.user_id', req.params.id)
     .then(function (result) {
       res.json(result);
@@ -66,14 +120,9 @@ router.get('/user/:id', function(req, res) {
 router.post('/', function(req, res){
 
   knex('shindig_request').insert({
-    name: req.body.name,
-    capacity: req.body.capacity,
-    hour_rate: req.body.hour_rate,
-    day_rate: req.body.hour_rate,
-    img_url: req.body.img_url,
-    available: req.body.available,
-    venue_id: knex('venue').where('name', req.body.name).select('id'),
-  }, 'id').then(function(result){
+    shindig_id: req.body.shindig_id,
+    user_id: req.body.user_id
+  }, 'user_id').then(function(result){
     res.json(result);
   });
 });
